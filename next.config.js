c39296d8db9fea83dcdda09eea71d25d8e0b0a94
chandelyash02 <chandel.yash2@gmail.com>
2021-05-15 12:04:38 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports ={
     env:{
         SENDGRID_API:"https://api.sendgrid.com/v3/mail/send",
-        SENDGRID_API_KEY="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",
+        SENDGRID_API_KEY:"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",
         MONGO_URI:"mongodb+srv://yash:EDkvHBCpPQNyZUZd@cluster0.mm5yv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     }
 }
